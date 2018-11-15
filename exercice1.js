0a23270d78a3780d2175fb98e4bc36401b9f274d
Amandine <amandine.labaeye@orange.fr>
2018-11-15 09:55:05 +0100

    commit

--- exercice1.js
+++ exercice1.js
@@ -6,13 +6,31 @@ var monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 //Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+var position = monTexte.indexOf('i');
 
+document.getElementById('position').innerHTML = position;
 
+var Majuscule = monTexte.toUpperCase();
+
+document.getElementById('chaineMaj').innerHTML = Majuscule;
+
+for (var x = 0; x < monTexte.length; x++) {
+
+   var lettrea = 'a';
+
+   if (monTexte[x] === lettrea) {
+
+       alert('lettre a trouvé au caractère n°' + x);
+
+   }
+
+   console.log(lettrea);
+}
 
 
 
